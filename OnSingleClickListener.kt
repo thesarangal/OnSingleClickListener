a17964bc0b478eb4b4681bbe51c2cb6a94903e57
Sarangal <46309117+thesarangal@users.noreply.github.com>
2021-12-31 20:58:42 +0530

    OnSingleClickListener.kt File Added

---
+++ OnSingleClickListener.kt
@@ -0,0 +1,40 @@
+package com.relinns.medoplus.utills.extension
+
+import android.view.View
+import java.util.*
+
+/**
+ * View Extension Function to prevent double click
+ *
+ * @author Rajat Sarangal
+ * @since December 31, 2021
+ * */
+
+// Minimum Interval Time Between Next Click
+const val MIN_CLICK_INTERVAL_IN_MILLI = 500
+
+// Time of last click
+var lastUserClickOn = 0L
+
+fun View.setOnSingleClickListener(listener: View.OnClickListener?) {
+
+    // Register Generic Callback when this view is clicked.
+    setOnClickListener {
+
+        // Check is user click before safety interval
+        if(Date().time - lastUserClickOn < MIN_CLICK_INTERVAL_IN_MILLI){
+
+            // Store User's Last Click
+            lastUserClickOn = Date().time
+
+            return@setOnClickListener
+        }
+
+        // Store User's Last Click
+        lastUserClickOn = Date().time
+
+        // Invoke Callback
+        listener?.onClick(it)
+    }
+
+}
